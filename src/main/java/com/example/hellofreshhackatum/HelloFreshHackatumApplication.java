fe7de591c020efdf7c0423443a0843dc8499570b
Raphael Dichler <t-rdichler@microsoft.com>
2023-11-18 08:42:34 +0100

    init spring

---
+++ src/main/java/com/example/hellofreshhackatum/HelloFreshHackatumApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellofreshhackatum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFreshHackatumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloFreshHackatumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellofreshhackatum/HelloFreshHackatumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellofreshhackatum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloFreshHackatumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
